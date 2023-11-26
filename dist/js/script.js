5c37d257a841c0d6ac1e8722917dd2ce1e8ba470
shomurodovx <jamshidedits@gmail.com>
2023-11-26 13:08:09 +0500

    dark mode qoshildi

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,30 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const light = document.querySelector(".light");
+
+const userTheme = localStorage.getItem("theme");
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const themeCheck = () => {
+  if (userTheme === "dark" || (!userTheme && systemTheme)) {
+    document.documentElement.classList.add("dark");
+  }
+};
+
+const themeSwitch = () => {
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+    return;
+  }
+  document.documentElement.classList.add("dark");
+  localStorage.setItem("theme", "dark");
+};
+
+light.addEventListener("click", () => {
+  themeSwitch();
+});
+
+themeCheck();
